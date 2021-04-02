b34339b02742d05fe4e2314b754049d630c33b1d
root <root@panther.ce.ou.edu>
2021-04-02 15:17:40 -0500

    Missed remove_next_prev_buttons.js

---
+++ canvas_js/remove_next_prev_buttons.js
@@ -0,0 +1,14 @@
+// Remove prev / next default canvas navigation buttons
+
+$(function(){
+    // Will Poillion 2020-09-09
+    // Switched to use vanilla JS so as not to depend on jquery
+    //$('.module-sequence-footer').remove();
+    
+    var navigationFooter = document.getElementsByClassName("module-sequence-footer");
+    if (navigationFooter != undefined && navigationFooter[0] != undefined) {
+        navigationFooter[0].remove();
+    }
+    
+    console.log("Module Navigation Removed.");
+});
